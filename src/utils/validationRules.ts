3423699899e49ed7b7e3d574dcd502fc6a64a995
Никита <nikata.shatalov.04@mail.ru>
2025-11-18 23:57:54 +0300

    add validation rules (numeric, ageRange, name)

--- src/utils/validationRules.ts
+++ src/utils/validationRules.ts
@@ -1,16 +1,39 @@
 import type { ValidationRule } from "@/types/validation";
 
 export const required: ValidationRule = (value) => {
-    return value.trim() .length > 0 || "Обязательное поле";
-}
+  return value.trim().length > 0 || "Обязательное поле";
+};
 
 export const email: ValidationRule = (value) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(value) || "Неправильный формат email";
-}
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(value) || "Неправильный формат email";
+};
 
 export const minLength = (min: number): ValidationRule => {
-    return (value) => {
-        return value.length >= min || `Минимальная длина ${min} символов`;
-    };
+  return (value) => {
+    return value.length >= min || `Минимальная длина ${min} символов`;
+  };
+};
+
+export const numeric: ValidationRule = (value) => {
+    const isNumber = !isNaN(Number(value));
+    return isNumber || "Только цифры";
+}
+
+export const ageRange: ValidationRule = (value) => {
+  const age = Number(value);
+
+  if (age < 12) {
+    return "Возраст должен быть не меньше 12 лет";
+  }
+
+  if (age > 100) {
+    return "Возраст должен быть не больше 100 лет";
+  }
+
+  return true;
+};
+
+export const name: ValidationRule = (value) => {
+  return value.length >= 2 || 'Минимум 2 символа'
 }
