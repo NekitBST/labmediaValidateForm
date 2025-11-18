f3357a9773e834213deff86626cfde5dfa24b138
Никита <nikata.shatalov.04@mail.ru>
2025-11-19 00:49:28 +0300

    add function validateField

--- src/composables/useFormValidation.ts
+++ src/composables/useFormValidation.ts
@@ -17,7 +17,26 @@ export function useFormValidation(config: FormConfig) {
     
     const fields = reactive(fieldsData) as FormFields
 
+    const validateField = (fieldName: string) => {
+        const value = fields[fieldName].value
+        const rules = config[fieldName]
+        const errors: string[] = []
+        
+        for (const rule of rules) {
+            const result = rule(value)
+            
+            if (result !== true) {
+                errors.push(result as string)
+            }
+        }
+        
+        fields[fieldName].errors = errors
+        fields[fieldName].isValid = errors.length === 0
+        fields[fieldName].touched = true
+        
+    }
     return {
-        fields
+        fields,
+        validateField
     }
 }
