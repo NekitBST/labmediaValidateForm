c09d2e21704b044e2dc986a3568efbe140c8f64e
Никита <nikata.shatalov.04@mail.ru>
2025-11-19 14:21:45 +0300

    create HTTP request types

---
+++ src/types/http.ts
@@ -0,0 +1,16 @@
+export type HttpMethod = "GET" | "POST";
+
+export interface HttpOptions {
+    method?: HttpMethod;
+    headers?: Record<string, string>;
+    body?: any;
+}
+
+export interface HttpState<T = any> {
+    data: T | null
+    status: number | null
+    isLoading: boolean
+    isSuccess: boolean
+    isError: boolean
+    error: string | null
+}
